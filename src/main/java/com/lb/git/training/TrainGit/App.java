2e422334d31b62c86a58119f57472cc589af5c2b
Saurabh Agarwal <saurabh.agarwal@lochbridge.com>
2017-01-24 23:10:43 +0530

    added sysout

--- src/main/java/com/lb/git/training/TrainGit/App.java
+++ src/main/java/com/lb/git/training/TrainGit/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "How are you!" );
+        System.out.println( "Saurabh Agarwal!!!!" );
     }
 }
